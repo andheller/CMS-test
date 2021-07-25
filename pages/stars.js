0f842f2e800f8b595566c8ba5a0d214ff8312a8d
andheller <aheller9@gmail.com>
2021-07-25 14:27:53 -0500

    1 file modified, 2 deleted

--- pages/stars.js
+++ pages/stars.js
@@ -2,10 +2,10 @@ function Page({ stars }) {
   return <div>Next stars: {stars}</div>;
 }
 
-Page.getInitialProps = async ctx => {
+Page.getStaticProps = async ctx => {
   const res = await fetch('https://cofocus-dev-v1.vercel.app/api/content');
   const json = await res.json();
-  return { stars: json.component };
+  return { stars: json.component,  revalidate: 10, //  };
 };
 
 export default Page;
